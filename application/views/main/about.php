5b018da3e14a1a3d1e45cd98e68766055ab9086d
Farahnush <nfarahnush@gmail.com>
2021-08-16 09:38:04 -0400

    sync

---
+++ application/views/main/about.php
@@ -0,0 +1,22 @@
+<header class="masthead" style="background-image: url('/public/images/about-bg.jpg')">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 col-md-10 mx-auto">
+                <div class="page-heading">
+                    <h1>Faramateo's blog</h1>
+                    <span class="subheading">Образовательный блог по изучению английского</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+<div class="container">
+    <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+            <p>Привет! Я - Фарахнуш, а Faramateo мой творческий псевдоним. Рада приветствовать тебя на моем блоге!
+                Мой блог про образование, изучение английского языка и саморазвитие. Вместе с тобой буду доводить свой
+                английский до совершенства. :)
+                </p>
+        </div>
+    </div>
+</div>
